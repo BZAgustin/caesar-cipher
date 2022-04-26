6d51b25ffa901b394a57c3feedd649e0b139eed5
BZAgustin <abustoszanelli@gmail.com>
2022-04-26 11:35:52 -0300

    - Add Caesar Cipher project

---
+++ caesar-cipher.rb
@@ -0,0 +1,45 @@
+# The program takes 2 arguments that should be input from the user: A string and a number (The text to encrypt and the shift factor)
+# The string is then divided into single array objects and the program iterates over each of them to shift them by the factor indicated by
+# the user (173 > char > 100)
+
+def caesar_cipher(str, factor)
+    # Convert the string to an array of ASCII values with #bytes
+    asciiChars = str.bytes
+
+    # Shift the characters by the factor indicated, ignoring the ones that are not
+    if asciiChars.any? { |char| char >= 65 && char <= 90 || char >= 97 && char <= 122 }
+        asciiChars = asciiChars.map do |char| 
+            if char >= 65 && char <= 90 || char >= 97 && char <= 122
+                char += factor
+            else
+                char
+            end
+        end
+    end
+
+    # Check if any shifted character goes past 'Z'. Wrap it around if it does
+    if asciiChars.any? { |char| char > 122 || char > 90 && char < 97}
+       asciiChars = asciiChars.map do |char|
+            if char > 122
+                char = 96 + (char-122)
+            elsif char > 90 && char < 97
+                char = 64 + (char-90)
+            else
+                char
+            end
+        end
+    end
+
+    asciiChars = asciiChars.map { |char| char = char.chr }
+
+    asciiChars.join('')
+end
+
+puts "Enter a string: "
+strInput = gets.chomp
+
+puts "Enter a shift factor (number):"
+numInput = gets.chomp
+numInput = numInput.to_i
+
+puts caesar_cipher(strInput, numInput)
